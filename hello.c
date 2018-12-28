146c32a6827c14cacf5104d16df1e64a9fcd393d
yan <645786233@qq.com>
2018-12-28 17:51:19 +0800

    first practice

---
+++ hello.c
@@ -0,0 +1,13 @@
+// 预处理指令，指定要包含的头文件
+#include <stdio.h>
+
+// 主函数入口
+int main()
+{
+    // 控制台输出语句
+    printf("Hello C world!\n");
+    printf("Hello visual Studio");
+    
+    // 执行完毕，终止运行
+    return 0;
+}
